fd2a1ed44908b1c35d16a73524ad26776d9ca8a4
Jesus Valentin Mendoza Torres <jmendozat13@gmail.com>
2021-02-01 22:53:13 -0500

    Se agrega device al interceptor

--- data/src/main/java/com/afoxplus/data/di/providers/ApiInterceptor.kt
+++ data/src/main/java/com/afoxplus/data/di/providers/ApiInterceptor.kt
@@ -3,11 +3,14 @@ package com.afoxplus.data.di.providers
 import okhttp3.Interceptor
 import okhttp3.Response
 
+private const val DEVICE_MODEL = "device"
 
 class ApiInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
-        request = request.newBuilder().build()
+        request = request.newBuilder()
+            .addHeader(DEVICE_MODEL, "${android.os.Build.MANUFACTURER} ${android.os.Build.MODEL}")
+            .build()
         return chain.proceed(request)
     }
 }
