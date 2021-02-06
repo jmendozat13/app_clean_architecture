bc339ec4da7823b4150b89cbc2365bd2d5540b14
Jesus Valentin Mendoza Torres <jmendozat13@gmail.com>
2021-02-06 15:08:46 -0500

    Se modifica dependencia de Dispatcher

--- domain/src/main/java/com/afoxplus/domain/di/UseCaseModule.kt
+++ domain/src/main/java/com/afoxplus/domain/di/UseCaseModule.kt
@@ -1,10 +1,8 @@
 package com.afoxplus.domain.di
 
 import com.afoxplus.domain.usecases.ChatBotUseCase
-import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 
 val useCaseModule = module {
-    single { Dispatchers.IO }
     single { ChatBotUseCase() }
 }
--- domain/src/main/java/com/afoxplus/domain/usecases/ChatBotUseCase.kt
+++ domain/src/main/java/com/afoxplus/domain/usecases/ChatBotUseCase.kt
@@ -2,16 +2,15 @@ package com.afoxplus.domain.usecases
 
 import com.afoxplus.domain.entities.ChatBot
 import com.afoxplus.domain.repository.IChatBotRepository
-import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.koin.core.inject
 
 class ChatBotUseCase : BaseUseCase() {
 
     private val chatBotRepository: IChatBotRepository by inject()
-    private val dispatcher: CoroutineDispatcher by inject()
 
-    suspend fun sendMessage(inputMessage: String): ChatBot = withContext(dispatcher) {
+    suspend fun sendMessage(inputMessage: String): ChatBot = withContext(Dispatchers.IO) {
         chatBotRepository.sendMessage(inputMessage)
     }
 }
