987a7cdf5859316bf8c7b913492b5413a0153f99
Jesus Valentin Mendoza Torres <jmendozat13@gmail.com>
2021-02-06 13:46:50 -0500

    Se agrega libreria room

--- app/src/main/java/com/afoxplus/appdemo/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/afoxplus/appdemo/ui/splash/SplashActivity.kt
@@ -5,19 +5,13 @@ import androidx.lifecycle.lifecycleScope
 import com.afoxplus.appdemo.databinding.ActivitySplashBinding
 import com.afoxplus.appdemo.ui.BaseActivity
 import com.afoxplus.appdemo.ui.main.MainActivity
-import kotlinx.android.synthetic.main.activity_splash.view.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-//val = inmutable
-//var = mutable
-
 class SplashActivity : BaseActivity() {
 
-    //private var myVariable: String? = null
-
-    private lateinit  var bindingSplashActivity: ActivitySplashBinding
+    private lateinit var bindingSplashActivity: ActivitySplashBinding
     override fun onCreate() {
         bindingSplashActivity = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(bindingSplashActivity.root)
@@ -26,17 +20,6 @@ class SplashActivity : BaseActivity() {
             delay(4000)
             finish()
             MainActivity.start(this@SplashActivity)
-            //hacerAlgo(myVariable)
         }
     }
-
-    /*private fun hacerAlgo(cadena: String?) {
-        cadena?.let { nuevaVariable: String ->
-            println(nuevaVariable)
-        } ?: println("Es nulo")
-        // let - it
-        // run
-        // apply
-        // when
-    }*/
 }
