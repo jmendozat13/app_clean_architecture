4b0d8d30fa958b4a4a2daa43c055d70905a4659f
Valentin Mendoza <jmendozat13@gmail.com>
2022-07-12 22:34:44 -0500

    added links

--- app/src/main/java/com/afoxplus/appdemo/ui/chat/viewholder/ChatResponseViewHolder.kt
+++ app/src/main/java/com/afoxplus/appdemo/ui/chat/viewholder/ChatResponseViewHolder.kt
@@ -1,5 +1,6 @@
 package com.afoxplus.appdemo.ui.chat.viewholder
 
+import android.text.util.Linkify
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.afoxplus.appdemo.databinding.RowChatResponseBinding
@@ -7,6 +8,7 @@ import com.afoxplus.appdemo.ui.chat.adapter.ChatBotOptionAdapter
 import com.afoxplus.appdemo.ui.chat.adapter.OnClickMessageListener
 import com.afoxplus.domain.entities.account.User
 import com.afoxplus.domain.entities.chat.Message
+import me.saket.bettermovementmethod.BetterLinkMovementMethod
 
 class ChatResponseViewHolder private constructor(
     private val bindingResponse: RowChatResponseBinding,
@@ -19,6 +21,11 @@ class ChatResponseViewHolder private constructor(
     override fun bind(message: Message) {
         message.replaceUserName(user)
         bindingResponse.message = message
+        Linkify.addLinks(
+            bindingResponse.responseTextMessage,
+            Linkify.PHONE_NUMBERS or Linkify.WEB_URLS
+        )
+        bindingResponse.responseTextMessage.movementMethod = BetterLinkMovementMethod.getInstance()
         bindingResponse.adapterOption = adapterOptions
         adapterOptions.setOnClickOptionListener { option ->
             onClickMessage.onClick(option)
